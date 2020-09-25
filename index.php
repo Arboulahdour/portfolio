0f86576206fa15fb87ee6bc1c6b2c5ba3a32d9ef
Abderrahmane Boulahdour <58618795+Arboulahdour@users.noreply.github.com>
2020-09-25 16:00:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -214,12 +214,12 @@
                 </div>
                 <div class="content-info-1">
                     <div class="container"> 
-                          <form id="contact" action="" method="post">
+                          <form id="contact" action="/email.php" method="post">
                             <fieldset>
                               <input placeholder="Your name" type="text" tabindex="1" required>
                             </fieldset>
                             <fieldset>
-                              <input placeholder="Your Email Address" type="email" tabindex="2" required>
+                              <input placeholder="Your Email Address" type="email" tabindex="2" name="email" required>
                             </fieldset>
                             <fieldset>
                               <input placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required>
@@ -228,7 +228,7 @@
                               <input placeholder="Your Web Site (optional)" type="url" tabindex="4" required>
                             </fieldset>
                             <fieldset>
-                              <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
+                              <textarea placeholder="Type your message here...." tabindex="5" name="commentaires" required></textarea>
                             </fieldset>
                             <fieldset>
                               <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
