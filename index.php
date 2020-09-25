2eb03b8622955b7cd0df62aca3eb528cf597a0b8
Abderrahmane Boulahdour <58618795+Arboulahdour@users.noreply.github.com>
2020-09-25 15:38:20 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Portfolio Abderrahmane Boulahdour</title>
+	<meta name="google-site-verification" content="8uHymH9Rmy-ng452Kl691P7rzHfN4ZYQYHpVtiVg_eM" />    
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="Content-Style-Type" content="text/css"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="Content-Language" content="en"/>
+		<meta name="author" content="Abderrahmane Boulahdour"/>
+		<meta name="KeyWords" content=""/>
+		<meta name="Description" content=""/>
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+    </head> 
+    <body> 
+    	<nav>
+            <input type="checkbox" id="check">
+            <label for="check" class="checkbtn">
+                <i class="fas fa-bars"></i>
+            </label>
+    		<label class="logo"><a href="#me" style="text-decoration: none; color: white;">Ar.Boulahdour</a></label>
+    		<ul>
+    			<li><a href="#about-me">About me</a></li>
+    			<li><a href="#experience">Projects</a></li>
+    			<li><a href="#educ">Skills</a></li>
+    			<li><a href="#certifs">Certifications</a></li>
+    			<li><a href="#scontact">Contact</a></li>
+    		</ul>
+    	</nav>
+        <a id="me"></a>
+        <section>
+            <div id="head">
+                    <img class="user-pic" src="pic.jpg" alt="user" />
+                    <div class="head-title">
+                        Abderrahmane Boulahdour
+                    </div>
+                    <div class="head-line-sc">
+                        <div class="head-line"></div>
+                    </div>
+                    <div class="head-desc">
+                        <div>
+                            IT Engineer (CCNA R&S | JNCIA x4 | NSE x2 | dCAA Asterisk | CyberArk)
+                        </div>
+                        
+                    </div>
+                    <div id="about-sc">
+                        <a href="https://www.linkedin.com/in/abderrahmane-boulahdour-462165169" target="_blank"><i class="fab fa-linkedin fa-2x" style="margin-right: 10px;"></i></a>
+                <a href="https://web.facebook.com/billie.abdou.35/" target="_blank"><i class="fab fa-facebook fa-2x" style="margin-right: 10px;"></i></a>
+                <a href="https://twitter.com/ABoulahdour" target="_blank"><i class="fab fa-twitter fa-2x" style="margin-right: 10px;"></i></a>
+                <a href="https://www.instagram.com/ar.boulahdour/?hl=en" target="_blank"><i class="fab fa-instagram fa-2x" style="margin-right: 10px;"></i></a>
+                <a href="https://arboulahdour.github.io/info" target="_blank"><i class="fas fa-globe fa-2x" style="margin-right: 10px;"></i></a>
+                    </div>
+            <a id="about-me"></a>
+            </div>
+            <div class="content">
+                <div class="content-title">
+                    About me
+                </div>
+                <div class="content-line-sc">
+                        <div class="content-line-ab"></div>
+                </div>
+                <div class="content-info-1">
+                    <div class="about">
+                        Hey! I am an IT Engineer certified from multi-vendors such as Cisco, Juniper and so on. I am very motivated and dynamic, I like to learn and to share knowledge. I am interested in Networking and Systems and more than interested in Cloud, DevOps, Cybersecurity and Project Management.
+                        <br><br>
+                        Scroll down to see more ;)
+                    </div>
+                </div>
+                <a id="experience"></a>
+                <br><br>
+            </div>
+            <div class="content-ex">
+                <div class="content-title">
+                    PROJECTS
+                </div>
+                <div class="content-line-sc">
+                        <div class="content-line-pr"></div>
+                </div>
+                <div class="content-info-1">
+                    <!-- Full-width images with number and caption text -->
+                <div class="about">
+                    <div class="mySlides fade">
+                       <div class="numbertext"></div>
+                       <h2><i class="fa fa-arrow-right fa-1x" style="margin-right: 15px;"></i>Network Automation application « Net-Auto » using Django + Python</h2>
+                        <br>
+                        <ol>
+                            <li>This application offers some basic services to simplify the daily tasks, functions and repetitive processes of a network or system engineer. For more detail visit this <a href="https://www.linkedin.com/posts/abderrahmane-boulahdour-462165169_django-python-networkautomation-activity-6687724211017986048-XOcI/" style="color: rgba(0,0,0,.75);">link<i class="fas fa-external-link-alt fa-1x" style="margin-left: -5px;"></i></a> 
+                            </li> 
+                        </ol>
+                        <br>
+                    </div>
+                    <div class="mySlides fade">
+                       <div class="numbertext"></div>
+                      <h2><i class="fa fa-arrow-right fa-1x" style="margin-right: 15px;"></i>Application to create VPS remotely using Django + REST API + Ansible</h2>
+                        <br>
+                        <ol>
+                            <li>This application allows to create VPS as containers with a specific resources directly via a web page, it could be usefull for companies that provide cloud services. For more detail visit this <a href="https://www.linkedin.com/posts/abderrahmane-boulahdour-462165169_django-python-webdevelopment-activity-6691722600571289600-GG61/" style="color: rgba(0,0,0,.75);">link<i class="fas fa-external-link-alt fa-1x" style="margin-left: -5px;"></i></a> 
+                            </li> 
+                        </ol>
+                    </div>
+                    <div class="mySlides fade">
+                      <div class="numbertext"></div>
+                          <h2><i class="fa fa-arrow-right fa-1x" style="margin-right: 15px;"></i>Basic chat application using Django + jQuery (JS)</h2>
+                        <br>
+                        <ol>
+                            <li>This web application allows to communicate with multiple people simultaneously and in real time. For more detail visit this <a href="https://www.linkedin.com/posts/abderrahmane-boulahdour-462165169_django-javascript-jquery-activity-6696813902681960448-43t5/" style="color: rgba(0,0,0,.75);">link<i class="fas fa-external-link-alt fa-1x" style="margin-left: -5px;"></i></a> 
+                            </li> 
+                        </ol>
+                    </div>
+                </div>
+                    <!-- Next and previous buttons -->
+                <!--     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                     <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                        <br> -->
+                    <!-- The dots/circles -->
+                    <div style="text-align:center">
+                      <span class="dot" onclick="currentSlide(1)"></span>
+                      <span class="dot" onclick="currentSlide(2)"></span>
+                      <span class="dot" onclick="currentSlide(3)"></span>
+                </div>
+                <!--<input type='button' value='slides' onclick='count();'/>-->
+                <script>
+                    var slideIndex = 1;
+                    showSlides(slideIndex);
+                    function plusSlides(n) {
+                                showSlides(slideIndex += n);
+                    }
+                    function currentSlide(n) {
+                                showSlides(slideIndex = n);
+                    }
+                    function showSlides(n) {
+                                var i;
+                                var slides = document.getElementsByClassName("mySlides");
+                                var dots = document.getElementsByClassName("dot");
+                                if (n > slides.length) {slideIndex = 1}    
+                                if (n < 1) {slideIndex = slides.length}
+                                for (i = 0; i < slides.length; i++) {
+                                    slides[i].style.display = "none";  
+                                }
+                                for (i = 0; i < dots.length; i++) {
+                                    dots[i].className = dots[i].className.replace(" active", "");
+                                }
+                                slides[slideIndex-1].style.display = "block";  
+                                dots[slideIndex-1].className += " active";
+                    }
+                    //function count()
+                    //{
+                      //  var x = document.getElementsByClassName("mySlides");
+                       // alert(x.length);
+                    //}
+                </script>
+            </div>
+                <a id="certifs"></a>
+                <br><br>
+            </div>
+            <div class="content-2">
+                <div class="content-title">
+                    Certifications
+                </div>
+                <div class="content-line-sc">
+                        <div class="content-line-cer"></div>
+                </div>
+                <br>
+                <div class="content-info">
+                   <div class="certifs">
+                        <a href="https://www.youracclaim.com/badges/9e7a287e-9d51-4ae1-affb-a904ed3f6734/public_url" target="_blank"><img class="logo" src="Junos.jpg"></a><div class="logo-espace"></div>
+                        <a href="https://www.youracclaim.com/badges/c8c34755-6b74-4568-b2c2-c9d3eff34671/public_url" target="_blank"><img class="logo" src="JNCDA.png"></a><div class="logo-espace"></div>
+                        <a href="https://www.youracclaim.com/badges/fef346e5-ea38-4398-8b64-444ef21a0a03/public_url" target="_blank"><img class="logo" src="JNCIA-DevOps.png"></a><div class="logo-espace"></div>
+                        <a href="https://www.youracclaim.com/badges/4bc435f2-0bbc-421b-a93f-7b671fc33246/public_url" target="_blank"><img class="logo" src="JNCIA-Cloud.png"></a>
+                    </div>
+                    <div class="certifs">
+                        <a href="https://www.youracclaim.com/badges/343cfeab-ce70-4cbc-812d-178e89332d55/public_url" target="_blank"><img class="logo" src="OIP.jpg"></a><div class="logo-espace"></div>
+                        <a href="https://training.fortinet.com/mod/customcert/view.php?id=1595&action=download&userid=478141" target="_blank"><img class="logo" src="n1.jpg"></a><div class="logo-espace"></div>
+                        <a href="https://training.fortinet.com/mod/customcert/view.php?id=1595&action=download&userid=478141" target="_blank"><img class="logo" src="n2.jpg"></a><div class="logo-espace"></div>
+                        <a href="https://www.linkedin.com/posts/abderrahmane-boulahdour-462165169_my-first-certificate-in-2020-activity-6630793650169356289-cCT5" target="_blank"><img class="logo" src="ss.jpg"></a>
+                    </div>
+                </div>
+                <a id="educ"></a>
+                <br><br>
+            </div>
+            <div class="content-ss">
+                <div class="content-title">
+                    SKILLS
+                </div>
+                <div class="content-line-sc">
+                        <div class="content-line-sck"></div>
+                </div>
+                <div class="content-info-1">
+                    <div class="about">
+                        <div class="line-1">
+                           <div class="skills">Networking</div><div style="width: 20px;"></div>
+                           <div class="skills">Systems</div><div style="width: 20px;"></div>
+                           <div class="skills">Programming</div><div style="width: 20px;"></div>
+                           <div class="skills">Web Development</div>
+                        </div>
+                        <div class="line-1">
+                            <div class="skills">Cloud</div><div style="width: 20px;"></div>
+                           <div class="skills">DevOps</div><div style="width: 20px;"></div>
+                           <div class="skills">AI (ML)</div><div style="width: 20px;"></div>
+                           <div class="skills">CyberSecurity</div>
+                        </div>
+                   </div>
+                </div>   
+                <a id="scontact"></a>
+                <br><br>
+            </div>
+            <div class="content-3">
+                <div class="content-title">
+                    CONTACT
+                </div>
+                <div class="content-line-sc">
+                        <div class="content-line-cn"></div>
+                </div>
+                <div class="content-info-1">
+                    <div class="container"> 
+                          <form id="contact" action="" method="post">
+                            <fieldset>
+                              <input placeholder="Your name" type="text" tabindex="1" required>
+                            </fieldset>
+                            <fieldset>
+                              <input placeholder="Your Email Address" type="email" tabindex="2" required>
+                            </fieldset>
+                            <fieldset>
+                              <input placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required>
+                            </fieldset>
+                            <fieldset>
+                              <input placeholder="Your Web Site (optional)" type="url" tabindex="4" required>
+                            </fieldset>
+                            <fieldset>
+                              <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
+                            </fieldset>
+                            <fieldset>
+                              <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+                            </fieldset>
+                          </form>
+                    </div>
+                </div>
+                <a id="experience"></a>
+                <br><br>
+            </div>
+            <div class="footer">
+                    <br/>
+                <div style="color:white; font-size:16px; text-indent: 8px;">Copyright © 2020 ArBoulahdour.</div> 
+            </div>
+        </section>
+    </body>
